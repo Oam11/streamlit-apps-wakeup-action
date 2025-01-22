866781ad7996322fad383269b2d1dc7877d8b8ef
Oam V Bhanushali <113987734+Oam11@users.noreply.github.com>
2025-01-22 23:22:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # List of Streamlit App URLs
 
 STREAMLIT_APPS = [
-    "https://oam-b.me/AutoML/",
-    "https://oam-b.me/QueryStream/",
-    "https://oam-b.me/Documentor/"
+    "https://oam-automl.streamlit.app/",
+    "https://oam-querystream.streamlit.app/",
+    "https://documento.streamlit.app/"
 ]
